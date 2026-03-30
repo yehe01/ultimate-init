32aecd3934ff309e912d6446a65c29852b42a812
He Ye <yehe01@gmail.com>
2026-03-31 02:11:55 +0800

    Update

--- init.el
+++ init.el
@@ -1,110 +1,124 @@
+;;; --- Package Management ---
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa"  . "https://elpa.gnu.org/packages/")
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (package-initialize)
 
-;; Install use-package if not present
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
+(eval-when-compile (require 'use-package))
 (setq use-package-always-ensure t)
 
+;; Silence the native-comp warnings you were getting
+(setq native-comp-async-report-warnings-errors 'silent)
+
 ;;; --- Visuals & Theme ---
-(use-package nerd-icons)
+(use-package moe-theme :config (load-theme 'moe-dark t))
+(use-package powerline :config (powerline-center-theme)) ; Restored center-theme
+(use-package yascroll :config (global-yascroll-bar-mode 1))
+(use-package rainbow-delimiters :hook (prog-mode . rainbow-delimiters-mode))
 
-(use-package moe-theme
-  :config
-  (load-theme 'moe-dark t))
-
-(use-package powerline
-  :config
-  (powerline-default-theme))
-
-(use-package yascroll
-  :config
-  (global-yascroll-bar-mode 1))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;;; --- Navigation & Editing ---
+;;; --- Navigation & Editing (Restored your specific keys) ---
 (use-package goto-last-change
-  :bind ("C-x C-/" . goto-last-change))
+  :bind ("C-x C-\\" . goto-last-change)) ; Restored your specific C-\ key
 
+(use-package undo-tree
+  :config (global-undo-tree-mode))
+
+(use-package ace-jump-mode
+  :bind ("C-o" . ace-jump-mode)
+  :config (ace-jump-mode-enable-mark-sync))
+
+(use-package expand-region
+  :bind (("M-i" . er/expand-region)
+         ("M-S-<up>" . er/mark-inside-quotes)
+         ("M-S-<down>" . er/mark-inside-pairs)))
+
+;;; --- Completion (Helm & Auto-Complete) ---
 (use-package helm
-  :init
-  (setq helm-mode-fuzzy-match t
-        helm-completion-in-region-fuzzy-match t)
   :config
   (helm-mode 1)
   :bind (("M-x" . helm-M-x)
+         ("M-y" . helm-show-kill-ring)
          ("C-x C-f" . helm-find-files)
-         ("C-x b" . helm-mini)))
+         ("C-x C-b" . helm-buffers-list)
+         ("C-x C-r" . helm-recentf)
+         ("C-c <SPC>" . helm-all-mark-rings)))
 
-(use-package elscreen
-  :init
-  ;; Prevent elscreen from trying to load w3m and wl helpers
-  (setq elscreen-display-tab nil) 
-  (setq elscreen-tab-display-control nil)
+(use-package auto-complete
   :config
-  (elscreen-start))
-;;; --- File Management ---
-(use-package dirvish
-  :init
-  (dirvish-override-dired-mode)
-  :custom
-  (dirvish-mode-line-format
-   '(:left (sort symlink) :right (omitted index)))
-  (dirvish-attributes '(file-size vc-state git-msg nerd-icons))
-  ;; Terminal-friendly settings
-  (dirvish-use-header-line nil)
-  (dirvish-use-mode-line t)
+  (require 'auto-complete-config)
+  (ac-config-default)
+  (global-auto-complete-mode t)
+  (setq ac-dwim t))
+
+;;; --- Git ---
+(use-package git-gutter
   :config
-  (dirvish-peek-mode) ; Preview files in the right pane
-  (setq dirvish-layout-config '((0 0.3 0.4 0.3))) ; 3-column Miller columns
+  (global-git-gutter-mode t)
+  :bind (("C-c n" . git-gutter:next-hunk)
+         ("C-c l" . git-gutter:previous-hunk)))
 
-  :bind
-  (("C-x d" . dirvish)
- :map dirvish-mode-map
- ("a"   . dirvish-quick-access)
- ("f"   . dirvish-file-info-menu)
- ("y"   . dirvish-yank-menu)
- ("s"   . dirvish-quicksort)
- ("TAB" . dirvish-subtree-toggle)
- ("M-t" . dirvish-layout-toggle)
- ("M-b" . dirvish-history-jump)
- ("M-s" . dirvish-setup-menu)
- ("M-e" . dirvish-emerge-menu)))
+;;; --- Your Restored Custom Functions ---
 
-;;; --- Language Specifics ---
-(use-package paredit
-  :hook ((emacs-lisp-mode lisp-mode scheme-mode) . enable-paredit-mode))
+;; Line Movement
+(defun move-line-down ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion (forward-line) (transpose-lines 1))
+    (forward-line) (move-to-column col)))
 
-(use-package exec-path-from-shell
-  :if (memq window-system '(mac ns x))
-  :config
-  (exec-path-from-shell-initialize))
+(defun move-line-up ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion (forward-line) (transpose-lines -1))
+    (move-to-column col)))
 
-;;; --- Custom Settings ---
+(global-set-key (kbd "<C-M-down>") 'move-line-down)
+(global-set-key (kbd "<C-M-up>") 'move-line-up)
+
+;; Fast Vertical Navigation
+(global-set-key (kbd "M-U") (lambda () (interactive) (forward-line -10)))
+(global-set-key (kbd "M-D") (lambda () (interactive) (forward-line 10)))
+
+;; "Mimic Vim" Open Line
+(defun open-line-below ()
+  (interactive) (end-of-line) (newline) (indent-for-tab-command))
+(defun open-line-above ()
+  (interactive) (beginning-of-line) (newline) (forward-line -1) (indent-for-tab-command))
+
+(global-set-key (kbd "<C-return>") 'open-line-below)
+(global-set-key (kbd "<C-S-return>") 'open-line-above)
+
+;; Dired Improvements
+(defun dired-back-to-top () (interactive) (beginning-of-buffer) (dired-next-line 4))
+(with-eval-after-load 'dired
+  (define-key dired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top))
+
+;; Elisp Save Hack
+(defun ome-remove-elc-on-save ()
+  (make-local-variable 'after-save-hook)
+  (add-hook 'after-save-hook
+            (lambda ()
+              (if (file-exists-p (concat buffer-file-name "c"))
+                  (delete-file (concat buffer-file-name "c"))))))
+(add-hook 'emacs-lisp-mode-hook 'ome-remove-elc-on-save)
+
+;;; --- General Settings ---
 (setq inhibit-startup-screen t)
 (setq make-backup-files nil)
 (delete-selection-mode 1)
+(global-set-key (kbd "M-/") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-j") (lambda () (interactive) (join-line -1)))
 
+;; Terminal Transparency (for CachyOS terminal)
 (defun my/fix-terminal-transparency (&optional frame)
-  "Remove background color to let terminal transparency show through."
   (unless (display-graphic-p frame)
-    (set-face-background 'default "unspecified-bg" frame)
-    (set-face-background 'fringe "unspecified-bg" frame)))
-
-;; Apply to the current session
+    (set-face-background 'default "unspecified-bg" frame)))
 (my/fix-terminal-transparency)
-
-;; Apply to any new frames (like if you use emacsclient)
-(add-hook 'after-make-frame-functions 'my/fix-terminal-transparency)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
